9a4e0a157ea1e961c594710585b68b55ab79e861
Zeldy Friedmana <zeldyfriedman@gmail.com>
2023-10-12 14:03:01 -0400

    summary done

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,22 +1,17 @@
 import { Card } from '@twilio-paste/core';
 import { TipSelectContainer } from '../containers/TipSelectContainer';
 
-import { MenuItems } from './MenuItems';
-import { NewItemForm } from './NewItemForm';
-import { Summary } from './Summary';
-
-const items = [
-  { uuid: 1, name: 'Tofu Roast', price: 14, quantity: 1 },
-  { uuid: 2, name: 'Vegan Ham', price: 12, quantity: 1 }
-];
+import {NewItemFormContainer} from '../containers/NewItemFormContainer'
+import { MenuItemsContainer } from '../containers/MenuItemsContainer';
+import { SummaryContainer } from '../containers/SummaryContainer';
 
 const Calculator = () => {
   return (
     <Card>
-      <NewItemForm />
-      <MenuItems items={items} />
+      <NewItemFormContainer />
+      <MenuItemsContainer/>
       <TipSelectContainer />
-      <Summary />
+      <SummaryContainer />
     </Card>
   );
 };
---
+++ src/containers/SummaryContainer.js
@@ -0,0 +1,21 @@
+import { connect } from "react-redux";
+import { Summary } from "../components/Summary";
+
+const mapStatetoProps = (state) => {
+    const items = state.items;
+
+    const subtotal = items.reduce((sum, item)=> sum + item.price * item.quantity, 0) 
+
+    
+    const tipAmount = subtotal * (state.tipPercentage / 100);
+    
+    const total = subtotal + tipAmount;
+    
+    return {
+        subtotal,
+        tipAmount,
+        total
+    }
+}
+
+export const SummaryContainer = connect(mapStatetoProps)(Summary)
