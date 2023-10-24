a5b7f48f0508237f7544f5125874095a74a2e1fd
Zeldy Friedmana <zeldyfriedman@gmail.com>
2023-10-24 12:08:34 -0400

    fully functional

--- src/containers/MenuItemContainer.js
+++ src/containers/MenuItemContainer.js
@@ -1,13 +1,20 @@
 import { connect } from "react-redux"
 import { removeItem, updatePrice, updateQuantity } from "../store/items/actions"
 import MenuItem from "../components/MenuItem"
+import { selectItemTotal, selectSubtotal } from "../store/items/selectors"
+
+const mapStatetoProps = (state, ownProps)=>{
+    return {
+        total: selectItemTotal(state, ownProps),
+    }
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         remove: () => dispatch(removeItem(ownProps.uuid)),
         updatePrice: (price) => dispatch(updatePrice(ownProps.uuid, price)),
-        updateQuantity: (quantity) => dispatch(updateQuantity(ownProps.uuid, quantity))
+        updateQuantity: (quantity) => dispatch(updateQuantity(ownProps.uuid, quantity)),
     }
 }
 
-export const MenuItemContainer = connect(null, mapDispatchToProps)(MenuItem)
+export const MenuItemContainer = connect(mapStatetoProps, mapDispatchToProps)(MenuItem)
